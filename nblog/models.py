9b72ef95fa296ff6854b0bff888d08f7890d15e3
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-07-09 15:27:30 +0300

    secondo

--- nblog/models.py
+++ nblog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -8,6 +11,7 @@ class Post(models.Model):
     title=models.CharField(max_length=250)
     slug=models.SlugField(max_length=250)
     body=models.TextField()
+    author=models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
     publish=models.DateTimeField(default=timezone.now)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
@@ -17,7 +21,7 @@ class Post(models.Model):
     class Meta:
         ordering = ['-publish']
         indexes=[
-            models.Index(fields=['-publish'])
+            models.Index(fields=['-publish']),
         ]
     def __str__(self):
         return self.title  
