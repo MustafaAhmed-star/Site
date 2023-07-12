520f01f3f6eebd921958a168d0b00befd4e4863a
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-07-12 22:12:28 +0300

    modify templates

--- myblog/urls.py
+++ myblog/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',include('nblog.urls',namespace='nblog')),
+    path('blog/',include('nblog.urls',namespace='nblog')),
 ]
