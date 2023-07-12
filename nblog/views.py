63c7365b78044cb222472dd78d471628888b29f1
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-07-12 17:54:05 +0300

    add  list and detail views and routers map

--- nblog/views.py
+++ nblog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+from django.http import Http404
 # Create your views here.
+def post_list(request):
+    posts=Post.published.all()
+    context={
+        'posts':posts,
+    }
+    return render(request,'nblog/post/list.html',context=context)
+
+def post_detail(request,id):
+    post=get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
+    context={
+        'post':post,
+    }
+    
+    return render(request,'nblog/post/detail.html',context=context)
