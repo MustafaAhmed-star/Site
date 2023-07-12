9106f40d6a1ddfa0e45931ce458121148d1328a2
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-07-12 13:53:52 +0300

    bird

--- nblog/models.py
+++ nblog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -18,6 +21,8 @@ class Post(models.Model):
     status=models.CharField(max_length=2,
                             choices=Status.choices,
                             default=Status.DRAFT)
+    objects=models.Manager()
+    published=PublishedManager()
     class Meta:
         ordering = ['-publish']
         indexes=[
