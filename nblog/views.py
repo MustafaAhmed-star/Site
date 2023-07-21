7a05fb3af6fcba47f56e793d0039f51bc958a077
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-07-22 00:07:27 +0300

    add sednig emails and recommendations

--- nblog/views.py
+++ nblog/views.py
@@ -4,6 +4,9 @@ from django.http import Http404
 from django.core.paginator import Paginator,EmptyPage,\
                                                         PageNotAnInteger
 from django.views.generic import ListView
+##
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 # Create your views here.
 """
 def post_list(request):
@@ -39,3 +42,28 @@ def post_detail(request,year,month,day,post):
     }
     
     return render(request,'nblog/post/detail.html',context=context)
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'kkamen24@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    context = {
+        'post': post,
+        'form': form,
+        'sent': sent,
+    }
+    return render(request, 'nblog/post/share.html', context=context)
