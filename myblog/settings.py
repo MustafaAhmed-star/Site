c788ed911cab58443a31eaf27147a13fd491d6c1
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-07-24 00:26:30 +0300

    add taggit package

--- myblog/settings.py
+++ myblog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "nblog.apps.NblogConfig" ,
+    "nblog.apps.NblogConfig",
+    'taggit',
 ]
 
 MIDDLEWARE = [
--- nblog/models.py
+++ nblog/models.py
@@ -3,11 +3,13 @@ from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
+    tags=TaggableManager()
     class Status(models.TextChoices):
         DRAFT='DF','Draft'
         PUBLISHED='PB','Published'
