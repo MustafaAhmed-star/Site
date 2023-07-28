a78eac416c1afd715df848e7eae86cea0e73d410
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-07-28 19:47:26 +0300

    created a inclusion template tag and template tag with querysets

--- nblog/templatetags/blog_tags.py
+++ nblog/templatetags/blog_tags.py
@@ -1,8 +1,21 @@
 from django import template
 from ..models import Post
-
+from django.db.models import Count
 register= template.Library()
-
+#a simple template tag 
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+#a inclusion template tag
+@register.inclusion_tag('nblog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts= Post.published.order_by('-publish')[:count]
+    return {'latest_posts':latest_posts}
+
+#a template tag with querysets
+@register.simple_tag 
+def get_most_commented_posts(count=3):
+    return Post.published.annotate(
+                        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
