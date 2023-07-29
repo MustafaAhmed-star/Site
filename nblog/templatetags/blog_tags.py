564409b91f597516f9cada6c1ede756a68024654
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-07-29 20:33:16 +0300

    Creating a template filter to support Markdown syntax

--- nblog/templatetags/blog_tags.py
+++ nblog/templatetags/blog_tags.py
@@ -1,6 +1,10 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
+
+
 register= template.Library()
 #a simple template tag 
 @register.simple_tag
@@ -19,3 +23,8 @@ def get_most_commented_posts(count=3):
     return Post.published.annotate(
                         total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
+
+
+@register.filter(name="markdown")
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
